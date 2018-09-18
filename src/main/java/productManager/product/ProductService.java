9eb2d8bc90c6959924c3dde0b389d32dafa07e73
Juliana Faria <juliana.paulo@zup.com.br>
2018-09-18 14:25:16 -0300

    correção ProductService

--- src/main/java/productManager/product/ProductService.java
+++ src/main/java/productManager/product/ProductService.java
@@ -1,9 +1,13 @@
 package productManager.product;
 
+import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+
+@Service
 public class ProductService {
 
     private List<Product> products = new ArrayList<>(Arrays.asList(
